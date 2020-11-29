b383f576697d630e0accd78c4c71bfff4c7ed06b
Ciasterix <cwaligora97@gmail.com>
2020-11-29 02:28:59 +0100

    Reproducing tree from a list of tokens

--- utils.py
+++ utils.py
@@ -1,29 +1,41 @@
 # import tensorflow as tf
-import tokenize, io
+import io
+import tokenize
 
 
 class TreeTokenizer:
 
     def __init__(self, pset):
         self.pset = pset
-        terminals = [p.format() for p in self.pset.terminals[self.pset.ret]]
-        primitives = [p.name for p in self.pset.primitives[self.pset.ret]]
-        self.vocabulary = terminals + primitives
+        self.terminals = {t.name: t for t in pset.terminals[pset.ret]}
+        self.primitives = {p.name: p for p in pset.primitives[pset.ret]}
+        self.vocabulary = [*self.terminals.keys()] + [*self.primitives.keys()]
         self.tokens2id = {t: i for i, t in enumerate(self.vocabulary)}
         self.id2tokens = {i: t for t, i in self.tokens2id.items()}
         self.trans = str.maketrans({'(': ' ', ')': ' ', ',': ' '})
 
-    def tokenize(self, string_tree):
+    def _tokenize(self, string_tree):
         tokens_string = string_tree.translate(self.trans)
         tokens = list(
             tokenize.tokenize(io.BytesIO(tokens_string.encode()).readline))
         tokens = [t.string for t in tokens if t.string in self.vocabulary]
         return tokens
 
-    def tree_to_ids(self, string_tree):
-        tokens = self.tokenize(string_tree)
+    def tokenize_tree(self, string_tree):
+        tokens = self._tokenize(string_tree)
         ids = [self.tokens2id[t] for t in tokens]
         return ids
+
+    def reproduce_expression(self, tokens):
+        tokens_names = [self.id2tokens[t] for t in tokens]
+        print('tokens_names:', tokens_names)
+        expr = []
+        for tn in tokens_names:
+            if tn in self.terminals:
+                expr.append(self.terminals[tn])
+            if tn in self.primitives:
+                expr.append(self.primitives[tn])
+        return expr
 
     def validate_program(self, program):
         try:
@@ -35,15 +47,31 @@ class TreeTokenizer:
 
 if __name__ == "__main__":
     import benchmarks
+    from deap import gp
+
     pset = benchmarks.standard_boolean_pset(num_in=6)
     tokenizer = TreeTokenizer(pset)
-    print("tokens2id:", tokenizer.tokens2id)
-    print("id2tokens:", tokenizer.id2tokens)
+    print("Setup tokenizer:")
+    print("tokens2id:")
+    print(tokenizer.tokens2id)
+    print("id2tokens:")
+    print(tokenizer.id2tokens)
 
     s2 = 'nand(nand(IN3, IN1), or_(nand(and_(IN2, IN0),\
      or_(IN2, IN3)), nand(IN4, nand(IN5, 0))))'
 
-    print("Tokenizing second string")
+    print("\nTokenizing string")
     print(s2)
-    print(tokenizer.tokenize(s2))
-    print(tokenizer.tree_to_ids(s2))
+    print("Tokens names:")
+    print(tokenizer._tokenize(s2))
+    print("Tokens ids:")
+    tokens = tokenizer.tokenize_tree(s2)
+    print(tokens)
+
+    print("\nnReproducing Tree")
+    expr = tokenizer.reproduce_expression(tokens)
+    print("Expression")
+    print(expr)
+    print("Tree:")
+    tree = gp.PrimitiveTree(expr)
+    print(str(tree))
